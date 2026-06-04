41a2db13e90db54ff7d1ccf51a27b97f2462091a
houliabc <houliabc@gmail.com>
2026-06-04 20:20:37 +0800

    1

---
+++ 考研计算机/数据结构/code/2_line/Sqlist.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define INT_MAX_SIZE 10
+
+typedef int ElemType;
+
+typedef struct Sqlist
+{
+    ElemType *data; // 存储空间的首地址
+    int length;
+}Sqlist;
+
+
+int main() {
+    
+    return 0;
+}
